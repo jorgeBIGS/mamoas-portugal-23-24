477c04d140c7c7c33ac571b7ed969eef43c6f9cc
Jorge Garcia <jorgarcia@us.es>
2023-07-14 20:12:39 +0200

    Cambios para adaptar bboxes. Falta transformacion.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,47 +5,11 @@ import shutil
 import os
 from tqdm import tqdm
 import os
-from gdal import gdal
 
-def check_train(bounds, train):
-    result = []
 
-    for x, y in train:
-        if bounds.left <= x= bounds.
 
-    return result
 
-def geotiff_to_png(input_path, output_path=None, return_object=False):
-    """
-    Converts a GeoTIFF file to a PNG file or object. Specific to Skysatimages with 4 bands (blue, green, red, nir).
 
-    Args:
-        input_path (str): The file path of the input GeoTIFF file.
-        output_path (str, optional): The file path of the output PNG file. If not provided, PNG object is returned. Defaults to None.
-        return_object (bool, optional): Whether to return the PNG data as an object. If True, the output_path parameter will be ignored. Defaults to False.
-
-    Returns:
-        numpy.ndarray or None: If output_path is not provided and return_object is True, returns a 3D numpy array representing the PNG image. Otherwise, returns None.
-
-    """
-    # Open input file
-    dataset = gdal.Open(input_path)
-    output_types = [gdal.GDT_Byte, gdal.GDT_UInt16, gdal.GDT_Float32]
-
-    # Define output format and options
-    options = gdal.TranslateOptions(format='PNG', bandList=[3,2,1], creationOptions=['WORLDFILE=YES'], outputType=output_types[0])
-
-    # Translate to PNG
-    if output_path is not None:
-        gdal.Translate(output_path, dataset, options=options)
-        print(f'Successfully saved PNG file to {output_path}')
-
-    # Return PNG object
-    if return_object:
-        mem_driver = gdal.GetDriverByName('MEM')
-        mem_dataset = mem_driver.CreateCopy('', dataset, 0)
-        png_data = mem_dataset.ReadAsArray()
-        return png_data
 
 
 # adapted from https://fractaldle.medium.com/satellite-images-deep-learning-spacenet-building-segmentation-a5d145a81c33
@@ -106,12 +70,23 @@ def generate_tiles(tif:rasterio.io.DatasetReader, size:int, dst_dir:str):
                 result += dst_tile_path
     return result
 
+def check_train(bounds, train):
+    result = []
+
+    for bbox in train:
+        x, y = bbox[0], bbox[1]
+        if bounds.left <= x <= bounds.right and bounds.bottom <= y <= bounds.top:
+            result += bbox
+
+    return result
+
+
 def get_training(shapefile):
     result = []
     gdf = gpd.read_file(shapefile)
     
     for x in gdf.values:
-        result.append((x[1], x[2]))
+        result.append(x[3].bounds)
     return result
 
 
@@ -125,18 +100,19 @@ def mamoas_tiles(tif_name, shapefile, size=50):
 
     dst_image_dir = "data/tiles/"
 
-    generate_tiles(img, size, dst_image_dir)
+    #generate_tiles(img, size, dst_image_dir)
 
     tile_paths = os.listdir(dst_image_dir)
+    
 
     valid_tiles = []
 
     for each in tile_paths:
 
         img_tmp = rasterio.open(f"{dst_image_dir}/{each}")
-
+       
       
-
+        #print((img_tmp.width, img_tmp.height))
         rgb = img_tmp.read()
 
               
@@ -144,12 +120,13 @@ def mamoas_tiles(tif_name, shapefile, size=50):
         #https://rasterio.readthedocs.io/en/latest/quickstart.html
         
         bounding_boxes = check_train(img_tmp.bounds, training)
+        
 
-        if (rgb.sum()) > 0 and not bounding_boxes.isEmpty():
+        if (rgb.sum()) > 0 and len(bounding_boxes)>0:
             valid_tiles.append(each)
     
     for each in valid_tiles:
-        geotiff_to_png(f"{dst_image_dir}/{each}", f"data/valid_tiles_becario/{each}") 
+        #geotiff_to_png(f"{dst_image_dir}/{each}", f"data/valid_tiles_becario/{each}") 
         shutil.move(f"{dst_image_dir}{each}",f"data/valid_tiles/{each}")
     
     return result
@@ -157,7 +134,7 @@ def mamoas_tiles(tif_name, shapefile, size=50):
 if __name__ == '__main__':
     #https://mmdetection.readthedocs.io/en/latest/user_guides/train.html#coco-annotation-format
     #https://mmdetection.readthedocs.io/en/v2.2.0/tutorials/new_dataset.html
-    print(mamoas_tiles("data/combinacion.tif", "data/original/Mamoas-Laboreiro.shp", size=500))
+    print(mamoas_tiles("data/combinacion.tif", "data/original/Mamoas-Laboreiro.shp", size=200))
 
 
     
