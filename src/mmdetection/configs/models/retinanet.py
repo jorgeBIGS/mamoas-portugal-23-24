2226226821f93a597aec80ba748d4135df37f1b0
Jorge Garcia <jorgarcia@us.es>
2023-12-12 14:03:49 +0100

    Cambios en las configuraciones

--- src/mmdetection/configs/models/retinanet.py
+++ src/mmdetection/configs/models/retinanet.py
@@ -4,3 +4,15 @@ _base_ = [
     '../_base_/runtime/default_runtime.py',
     '../mamoas_detection.py'
 ]
+
+model = dict(
+    bbox_head=dict(num_classes=1),
+    train_cfg = dict(max_epochs=24, val_interval=1),
+    test_cfg=dict(
+        rcnn=dict(
+            score_thr=0.05, # Default:0.05
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100))
+)
+
+default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=-1))
