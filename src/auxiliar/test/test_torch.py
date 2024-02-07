34c069f6132cbf4a1ac7176aa727f6091a8f6b7c
Jorge Garcia <jorgarcia@us.es>
2024-02-07 19:53:47 +0100

    Cambios menores

--- src/auxiliar/test/test_torch.py
+++ src/auxiliar/test/test_torch.py
@@ -1,3 +1,16 @@
 import torch
-x = torch.rand(5, 3)
-print(x)
+import sys
+import os
+from subprocess import call
+print('_____Python, Pytorch, Cuda info____')
+print('__Python VERSION:', sys.version)
+print('__pyTorch VERSION:', torch.__version__)
+print('__CUDA RUNTIME API VERSION')
+#os.system('nvcc --version')
+print('__CUDNN VERSION:', torch.backends.cudnn.version())
+print('_____nvidia-smi GPU details____')
+call(["nvidia-smi", "--format=csv", "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
+print('_____Device assignments____')
+print('Number CUDA Devices:', torch.cuda.device_count())
+print ('Current cuda device: ', torch.cuda.current_device(), ' **May not correspond to nvidia-smi ID above, check visibility parameter')
+print("Device name: ", torch.cuda.get_device_name(torch.cuda.current_device()))
